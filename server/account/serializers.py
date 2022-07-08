60da3ae8ae941c8563208cc521e2d68eda9fa20c
notslip <notslip1991@gmail.com>
2022-07-08 18:46:56 +0300

    added field posts in userserialiser

--- server/account/serializers.py
+++ server/account/serializers.py
@@ -1,8 +1,11 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model, models
 
+from blog.models import Post
+
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
+    posts = serializers.HyperlinkedRelatedField(many=True, read_only=True, view_name="post-detail")
     class Meta:
         model = get_user_model()
         fields = ['url', 'id', 'username', 'email', 'groups', 'posts']
