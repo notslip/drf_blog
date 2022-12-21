6301bde8a88007a88a7b31d700730c1c8ca2b797
notslip <notslip1991@gmail.com>
2022-12-21 15:46:21 +0300

    added parent comment serialiser

--- server/comments/serializers.py
+++ server/comments/serializers.py
@@ -3,8 +3,15 @@ from rest_framework import serializers
 from comments.models import Comment
 
 
-class CommentSerializer(serializers.ModelSerializer):
+class ParrentCommentSerializer(serializers.ModelSerializer):
 
-     class Meta:
-         model = Comment
-         fields = ['text', 'author', 'date', 'post', 'parent_comment']
+    class Meta:
+        model = Comment
+        fields = ['id', 'text', 'author', 'date', 'post']
+
+
+class CommentSerializer(serializers.ModelSerializer):
+        parent_comment = ParrentCommentSerializer(read_only=True)
+        class Meta:
+            model = Comment
+            fields = ['id', 'text', 'author', 'date', 'post', 'parent_comment']
