cf89eb748708bc030783d1f81c7f135475377536
notslip <notslip1991@gmail.com>
2023-01-10 00:10:06 +0300

    add create method for comment seriaizer

--- server/comments/serializers.py
+++ server/comments/serializers.py
@@ -3,15 +3,15 @@ from rest_framework import serializers
 from comments.models import Comment
 
 
-class ParrentCommentSerializer(serializers.ModelSerializer):
-
+class ParentCommentSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField()
     class Meta:
         model = Comment
         fields = ['id', 'text', 'author', 'date', 'post']
 
 
 class CommentSerializer(serializers.ModelSerializer):
-        parent_comment = ParrentCommentSerializer(default=None)
+        parent_comment = ParentCommentSerializer()
         author = serializers.PrimaryKeyRelatedField(
             read_only=True,
             default=serializers.CurrentUserDefault()
@@ -21,6 +21,12 @@ class CommentSerializer(serializers.ModelSerializer):
             fields = ['id', 'text', 'author', 'date', 'post', 'parent_comment']
 
         def save(self, **kwargs):
-            """Include default for read_only `user` field"""
+            """Include default for read_only `author` field"""
             kwargs["author"] = self.fields["author"].get_default()
             return super().save(**kwargs)
+
+        def create(self, validated_data):
+            parent_comment_data = validated_data.pop('parent_comment')
+            parent_comment = Comment.objects.get(id=parent_comment_data['id'])
+            comment = Comment.objects.create(**validated_data, parent_comment=parent_comment)
+            return comment
